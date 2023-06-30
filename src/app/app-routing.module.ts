e81eb7725ef1672f75c19f8a108b385f53461c70
Christine <mwangimweruc@gmail.com>
2023-06-30 21:50:01 +0300

    properties

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { VendorsComponent } from './vendors/vendors.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { BuyersComponent } from './buyers/buyers.component';
 
 const routes: Routes = [
-  {path: '', component:LayoutComponent,}
+  {path: '', component:LayoutComponent,},
+  {path: 'buyers', component:BuyersComponent,},
+  {path: 'vendors', component:VendorsComponent,},
+  {path: 'vehicles', component:VehiclesComponent,},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,14 @@ import { MenuComponent } from './menu/menu.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { BuyersComponent } from './buyers/buyers.component';
+import { VendorsComponent } from './vendors/vendors.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
 
 import { DividerModule } from 'primeng/divider';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+import { ImageModule } from 'primeng/image';
 
 @NgModule({
   declarations: [
@@ -19,13 +24,18 @@ import { DividerModule } from 'primeng/divider';
     MenuComponent,
     TopbarComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    BuyersComponent,
+    VendorsComponent,
+    VehiclesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     DividerModule,
-
+    CardModule,
+    TagModule,
+    ImageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
